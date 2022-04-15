10e07c2052f8d4552cec35919756a840005b11d9
Milomir Dragović <hello@milomir.rs>
2022-04-15 11:12:36 +0200

    adds contstructors for class Car

--- Car.java
+++ Car.java
@@ -1,6 +1,19 @@
 public class Car {
     private String model;
     private int buildYear;
+    private Float consumption;
+
+    public Car() {
+        this.setModel("UNDEFIEND");
+        this.setConsumption(0.0f);
+        this.setBuildYear(0000);
+    }
+
+    public Car(String model, int buildYear, Float consumption) {
+        this.setModel(model);
+        this.setBuildYear(buildYear);
+        this.setConsumption(consumption);
+    }
 
     public void setModel(String modelValue) {
         this.model = modelValue;
@@ -18,9 +31,18 @@ public class Car {
         return this.buildYear;
     }
 
+    public void setConsumption(Float consumption) {
+        this.consumption = consumption;
+    }
+
+    public Float getConsumption() {
+        return this.consumption;
+    }
+
     public void printInConsole() {
         System.out.println("Model: " + this.getModel());
         System.out.println("Build year: " + this.getBuildYear());
+        System.out.println("Consuption is " + this.getConsumption() + "l per 100 kilometers");
         System.out.println();
     }
 }
--- Main.java
+++ Main.java
@@ -3,14 +3,21 @@ public class Main {
         System.out.println("Here we start.");
 
         Car audi = new Car();
+        audi.printInConsole();
         audi.setModel("Audi");
         audi.setBuildYear(2021);
+        audi.setConsumption(4.5f);
         audi.printInConsole();
 
         Car bmv = new Car();
         bmv.setModel("BMW");
         bmv.setBuildYear(2015);
+        bmv.setConsumption(5.1f);
         bmv.printInConsole();
+
+        Car lada = new Car("Lada", 1998, 10.3f);
+        lada.printInConsole();
+
 
     }
 }
